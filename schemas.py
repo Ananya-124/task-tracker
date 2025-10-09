e539babbfc181bcc22602596effc1f338032665a
Ananya124 <ananyaananyaa124@gmail.com>
2025-10-09 19:13:45 +0530

    new

---
+++ schemas.py
@@ -0,0 +1,15 @@
+from pydantic import BaseModel
+
+class TaskBase(BaseModel):
+    title: str
+    description: str
+    done: bool = False
+
+class TaskCreate(TaskBase):
+    pass
+
+class Task(TaskBase):
+    id: int
+
+    class Config:
+        orm_mode = True
